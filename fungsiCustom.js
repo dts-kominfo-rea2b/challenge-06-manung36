9c5316f965ba3ab7613084b787dcaa01020910e7
manung36 <manung36@yahoo.com>
2022-06-08 18:09:08 +0700

    Menambah tugas challenge Harun Azis

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require("fs");
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,53 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback)=> {
+  const semuaFile=[file1,file2,file3];
+  const hasil=[];
+  semuaFile.forEach((item)=> {
+    const dapatItem= new Promise ((resolve, reject) => {
+      fs.readFile(item, (error, dataJson)=> {
+        if (error) {
+          reject(error);
+          return;
+        }
+        const data=JSON.parse(dataJson);
+        let expectedString="";
+        
+        if (data?.message !== undefined) {
+          expectedString = data?.message;
+        }
+
+        
+        if (data?.length) {
+          data?.forEach((item) => {
+            if (item?.message !== undefined) {
+              expectedString = item?.message;
+            }
+
+            if (item?.data?.message !== undefined) {
+              expectedString = item?.data?.message;
+            }
+          });
+        }
+
+        word = expectedString.split(" ");
+
+        resolve(word[1]);
+      });
+    });
+    hasil.push(dapatItem);
+  });
+
+  
+  Promise.all(hasil)
+    .then((values) => {
+      fnCallback(null, values);
+    })
+    .catch((error) => {
+      fnCallback(error, null);
+    });
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
